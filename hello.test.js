25ee4f04c51d500835d43e9d25bf4d1dec1686f7
Rodrigo Medina <rodrigo.medina.neri@gmail.com>
2020-03-16 19:13:05 -0600

    Added  more tests

--- hello.test.js
+++ hello.test.js
@@ -3,3 +3,9 @@ const hello = require('./hello');
 test('outputs the correct string', () => {
   expect(hello()).toBe("Hello world!");
 });
+test('outputs the correct string x2', () => {
+  expect(hello()).toBe("Hello world!");
+});
+test('outputs the correct string x3', () => {
+  expect(hello()).toBe("Hello world!");
+});
